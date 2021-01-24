2b3309d22440e23342204a267ac95d92f53e9019
Johan Niklasson <johan.niklasson@klarna.com>
2021-01-25 00:59:13 +0100

    Bumped version to 0.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ for package in setuptools.find_packages():
 
 setuptools.setup(
     name="gcloud-functions-utils",
-    version="0.3.2",
+    version="0.4.0",
     author="Johan Niklasson",
     author_email="johan@niklasson.me",
     description="",
