ab1c4f499d19368ff4375bc88d14121b76cf5f02
Johan Niklasson <johan.niklasson@klarna.com>
2021-01-27 22:49:58 +0100

    Added support for local env check for secrets

--- gcloud_functions_utils/utils/secret_manager.py
+++ gcloud_functions_utils/utils/secret_manager.py
@@ -5,22 +5,27 @@ from google.cloud import secretmanager
 
 
 class SecretManager:
-    def __init__(self, project_id: str = None):
+    def __init__(self, project_id: str = None, check_env: bool = False):
+        self.check_env = check_env
+
         # Get the default project id
         self.project_id = project_id if project_id is not None else os.getenv("GCP_PROJECT")
 
         if self.project_id is None:
             raise Exception("Cannot find project id. Please provide one or export it to GCP_PROJECT")
 
-    def get(self, secret_id: str, version_id: Union[str, int] = "latest"):
         # Create the Secret Manager client.
-        client = secretmanager.SecretManagerServiceClient()
+        self.client = secretmanager.SecretManagerServiceClient()
+
+    def get(self, secret_id: str, version_id: Union[str, int] = "latest"):
+        if self.check_env and secret_id in os.environ:
+            return os.getenv(secret_id)
 
         # Build the resource name of the secret version.
         secret_resource_name = f"projects/{self.project_id}/secrets/{secret_id}/versions/{version_id}"
 
         # Access the secret
-        response = client.access_secret_version(name=secret_resource_name)
+        response = self.client.access_secret_version(name=secret_resource_name)
 
         # Return the secret
         payload = response.payload.data.decode("UTF-8")
